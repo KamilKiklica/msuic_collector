04c07f16fea5507db1951e4764fd36b5e54340b5
Patryczkov <patryczkov@gmail.com>
2019-02-26 11:04:47 +0100

    Added main func, show_all, show_artist

--- music_reports.py
+++ music_reports.py
@@ -1,6 +1,13 @@
 import os
 import sys
 
+def show_all():
+    print("")
+    with open('lista.txt', 'r') as outfile:
+        for i, line in enumerate(outfile):
+            lineSplitted = line.split(",")
+            print("{} . {} | {} | {} | {} | {}".format(
+                i + 1, lineSplitted[0], lineSplitted[1], lineSplitted[2], lineSplitted[3], lineSplitted[4]))
 
 def show_albums():
     print("")
@@ -11,12 +18,18 @@ def show_albums():
             print("{}. {} ".format(
                 i + 1,  lineSplitted[1]))
 
-# funkcjazmieniająca czas w formacie "minuty:sekundy" na "sekundy"
-def time_to_seconds(x):
+
+def show_time_range():
+    with open("lista.txt", "r") as outfile:
+        answer = input("")
+
+
+def time_to_seconds(x):  # funkcjazmieniająca czas w formacie "minuty:sekundy" na "sekundy"
     a = str(x)
     a = a.split(':')
     b = (int(a[0])*60)+int(a[1])
     return b
+
 
 def switchPlaces(tab, i, j):
     temp = tab[j]
@@ -48,8 +61,9 @@ def sort_lenght():
             lineToWrite = "{} | {} | {} | {} | {}".format(
                 HScoreTempList[i][0], HScoreTempList[i][1], HScoreTempList[i][2], HScoreTempList[i][3], HScoreTempList[i][4])
             f.write(lineToWrite)
-            
+
     return(HScoreTempList)
+
 
 def show_temp():
     print("")
@@ -60,7 +74,32 @@ def show_temp():
                 i + 1, lineSplitted[0], lineSplitted[1], lineSplitted[2], lineSplitted[3], lineSplitted[4]))
 
 
-show_albums()
-sort_lenght()
+def main():  # powinno printować menu z wyborem sortowania
+    print("1. Show albums"
+          "2. Sort by lenght"
+          "3. Find album by name"
+          "4. Find by genre"
+          "5. Find by artist"
+          "6. Show shortest/longest"
+          "7. Show whole informations"
+          )
+    user_input = input("Which option would you like to chose? <1-8>: ")
+    if user_input == "1":
+        show_albums()
+    if user_input == "2":
+        pass
+    if user_input == "3":
+        pass
+    if user_input == "4":
+        pass
+    if user_input == "5":
+        pass
+    if user_input == "6":
+        pass
+    if user_input == "7":
+        show_all()
 
-show_temp()
+
+if __name__ == "__main__":
+    main()
+
