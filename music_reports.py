b1668a9c7c6fc1ff3071b070f49bf562d2d1bb14
Patryczkov <patryczkov@gmail.com>
2019-02-26 14:49:07 +0100

    Searching by year, more bugs

--- music_reports.py
+++ music_reports.py
@@ -1,6 +1,60 @@
 import os
 import sys
 
+def time_to_seconds(x):  # funkcjazmieniająca czas w formacie "minuty:sekundy" na "sekundy"
+    a = str(x)
+    a = a.split(':')
+    b = (int(a[0])*60)+int(a[1])
+    return b
+
+def switchPlaces(tab, i, j):
+    temp = tab[j]
+    tab[j] = tab[i]
+    tab[i] = temp
+    return tab
+
+def make_list_of_list():   #make list of lists from file lista.txt
+    results = []
+    with open('lista.txt') as outfile:
+        for line in outfile:
+            results.append(line.strip().split(','))
+    return results
+
+
+def sort_lenght():
+    print("")
+    album_temp_list = []
+    with open('lista.txt', 'r') as outfile:
+        for i, line in enumerate(outfile):
+            lineSplitted = line.split(",")
+            album_temp_list.append(lineSplitted)
+
+        for i in range(0, len(album_temp_list)):
+            for j in range(0, len(album_temp_list)-1):
+                a = (album_temp_list[j][4])
+                b = (album_temp_list[j+1][4])
+                aa = time_to_seconds(a)
+                bb = time_to_seconds(b)
+                if aa > bb:
+                    album_temp_list = switchPlaces(album_temp_list, j, j+1)
+
+    with open('lista.txt', 'r') as outfile:
+        f = open("temp.txt", "w+")
+        for i, line in enumerate(album_temp_list):
+            lineToWrite = "{} | {} | {} | {} | {}".format(
+               album_temp_list[i][0], album_temp_list[i][1], album_temp_list[i][2], album_temp_list[i][3], album_temp_list[i][4])
+            f.write(lineToWrite)
+
+    return(album_temp_list)
+
+def show_temp(): #debug
+    print("")
+    with open('temp.txt', 'r') as outfile:
+        for i, line in enumerate(outfile):
+            lineSplitted = line.split(" | ")
+            print("{} . {} | {} | {} | {} | {}".format(
+                i + 1, lineSplitted[0], lineSplitted[1], lineSplitted[2], lineSplitted[3], lineSplitted[4]))
+
 def show_all():
     print("")
     with open('lista.txt', 'r') as outfile:
@@ -19,83 +73,45 @@ def show_albums():
                 i + 1,  lineSplitted[1]))
 
 
-def show_time_range():
-    with open("lista.txt", "r") as outfile:
-        answer = input("")
+def show_time_range():          
+    album_temp_list = sort_lenght()
+    print("What time range of albums release would you like to see?")
+    start_date = input("Enter start date: ")
+    end_date = input("Enter end date: ")
+    for i in range(len(album_temp_list)):
+        if start_date <= album_temp_list[i][2] <= end_date:
+            print(album_temp_list[i][0], album_temp_list[i][1])
+
+def search_by_name():
+    album_temp_list = sort_lenght()
+    search = input("Enter what atrist are you looking for: ")
+    if any(search in s for s in album_temp_list):
+        print(album_temp_list[i][0], album_temp_list[i][1])
 
 
-def time_to_seconds(x):  # funkcjazmieniająca czas w formacie "minuty:sekundy" na "sekundy"
-    a = str(x)
-    a = a.split(':')
-    b = (int(a[0])*60)+int(a[1])
-    return b
-
-
-def switchPlaces(tab, i, j):
-    temp = tab[j]
-    tab[j] = tab[i]
-    tab[i] = temp
-    return tab
-
-
-def sort_lenght():
-    print("")
-    HScoreTempList = []
-    with open('lista.txt', 'r') as outfile:
-        for i, line in enumerate(outfile):
-            lineSplitted = line.split(",")
-            HScoreTempList.append(lineSplitted)
-
-        for i in range(0, len(HScoreTempList)):
-            for j in range(0, len(HScoreTempList)-1):
-                a = (HScoreTempList[j][4])
-                b = (HScoreTempList[j+1][4])
-                aa = time_to_seconds(a)
-                bb = time_to_seconds(b)
-                if aa > bb:
-                    HScoreTempList = switchPlaces(HScoreTempList, j, j+1)
-
-    with open('lista.txt', 'r') as outfile:
-        f = open("temp.txt", "w+")
-        for i, line in enumerate(HScoreTempList):
-            lineToWrite = "{} | {} | {} | {} | {}".format(
-                HScoreTempList[i][0], HScoreTempList[i][1], HScoreTempList[i][2], HScoreTempList[i][3], HScoreTempList[i][4])
-            f.write(lineToWrite)
-
-    return(HScoreTempList)
-
-
-def show_temp():
-    print("")
-    with open('temp.txt', 'r') as outfile:
-        for i, line in enumerate(outfile):
-            lineSplitted = line.split(" | ")
-            print("{} . {} | {} | {} | {} | {}".format(
-                i + 1, lineSplitted[0], lineSplitted[1], lineSplitted[2], lineSplitted[3], lineSplitted[4]))
-
-
+                
 def main():  # powinno printować menu z wyborem sortowania
-    print("1. Show albums"
-          "2. Sort by lenght"
-          "3. Find album by name"
-          "4. Find by genre"
-          "5. Find by artist"
-          "6. Show shortest/longest"
-          "7. Show whole informations"
+    print("1. Show albums" + "\n"
+          "2. Find by year" + "\n"
+          "3. Find album by name" + "\n"
+          "4. Find by genre" + "\n"
+          "5. Find by artist" + "\n"
+          "6. Show shortest/longest" + "\n"
+          "7. Show whole informations" + "\n"
           )
     user_input = input("Which option would you like to chose? <1-8>: ")
     if user_input == "1":
         show_albums()
     if user_input == "2":
-        pass
+        show_time_range()
     if user_input == "3":
         pass
     if user_input == "4":
-        pass
+        search_by_name()
     if user_input == "5":
         pass
     if user_input == "6":
-        pass
+        sort_lenght()
     if user_input == "7":
         show_all()
 
